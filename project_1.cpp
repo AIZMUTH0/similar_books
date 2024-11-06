b5f47cb315b6a69b298ef53c5d6805b9c7eff1a1
AIZMUTH0 <Sagar.warlani708@gmail.com>
2024-11-06 20:00:39 +0530

    Initial commit

---
+++ project_1.cpp
@@ -0,0 +1,409 @@
+#include <filesystem>
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <iomanip> //input output manipulater(Table output).
+
+namespace file_system = std::filesystem;
+using namespace std;
+
+// Classes - Start
+
+// Book File Blue Print.
+class ParentFile
+{
+public:
+   // This class variables will be Filled And will be accesible.
+   int fileNumber = 0;
+   string fileName;
+   string filePath;
+
+   vector<string> words;
+   vector<int> counts;
+   float totalWords = 0;
+   vector<float> normalizeWords;
+
+   // temperary variables
+   vector<string> word_i;
+   map<string, int> unsorted_map;
+   vector<pair<string, int>> sorted_word_and_count;
+
+   // default constructor.
+   ParentFile() {};
+   // default destructor.
+   ~ParentFile() {};
+
+   // For Reciving : Required information(File : number , name and path) and Assigning.
+   void fileDetails_input(int i, vector<string> Names, vector<string> Paths);
+
+   // For step 1 : Reading File : Alphabet or Numeric characters and storing as capital letters Word in vector word_i.
+   void requireWords();
+
+   // For Step 2 : Removing Commonly occuring words.
+   void RemovingCommon();
+
+   // For Storing , words ,counts and calculated normal frequency and Top occuring words wise sorting.
+   void storingInClass();
+   // For Manully clearing data calling when class object is not in use.
+   void clear()
+   {
+      fileNumber = 0;
+      fileName.clear();
+      filePath.clear();
+      words.clear();
+      normalizeWords.clear();
+   }
+};
+
+// Step 4 Similarity Matrix : Creating
+struct similarityMatrix
+{
+public:
+   int i;
+   int j;
+   float frequency;
+};
+
+// Classes - End
+
+// Functions - Declarations - Start
+
+void bookDetails(vector<string> &file_name, vector<string> &file_access_paths, int &filesCount);
+
+// Step 3 Similaity Index :function:
+void similarityIndex(int i, vector<string> &w_i, vector<float> &n_i, int j, vector<string> &w, vector<float> &n_j, vector<similarityMatrix> &matrix);
+
+// similarity matrix, sorting function,Comparing part
+auto compareMatrix = [](const auto &matrix_1, const auto &matrix_2)
+{
+   return matrix_1.frequency > matrix_2.frequency;
+};
+
+// Step 1 :Top occuring words:sorting function,Comparing part
+bool comapareSecond(pair<string, int> &a, pair<string, int> &b);
+
+// Step 5 : Reporting : printing top ten pairs of text book
+void report(vector<similarityMatrix> matrix, vector<string> Book);
+
+// Function-Declarations-End
+
+// Main Function-starts
+
+int main()
+{
+   // Getting Require File Details and Storing in vectors
+   vector<string> file_names;
+   vector<string> file_access_paths;
+   int filesCount = 0;
+   int count = 0;
+   
+   //Finding data-Set
+   bookDetails(file_names, file_access_paths, filesCount);
+
+   // Step 4 : Similarity Matrix :Part: blank.
+   vector<similarityMatrix> similarity_matrix;
+
+   cout << "Please wait , processing starts it may require 7 to 8 minutes to finishes excecution ...  " << endl;
+
+   // Step 4 : Similarity Matrix, wise looping (Excluding self similarity)
+   // Creating,Sending,Processing,Comparing,Storing,Deleting and Repeating , for 2080 times for Required information.
+   for (int i = 0; i < filesCount; i++)
+   {
+      ParentFile *File_i = new ParentFile;
+
+      File_i->fileDetails_input(i, file_names, file_access_paths);
+
+      File_i->requireWords();
+      File_i->RemovingCommon();
+      File_i->storingInClass();
+
+      for (int j = 0; j < filesCount; j++)
+      {
+
+         if (i < j)
+         {
+
+            ParentFile *File_j = new ParentFile;
+
+            File_j->fileDetails_input(j, file_names, file_access_paths);
+
+            // Step 1 :
+            File_j->requireWords();
+            // Step 2 :
+            File_j->RemovingCommon();
+            File_j->storingInClass();
+
+            // Step 3 :
+            similarityIndex(i, File_i->words, File_i->normalizeWords, j, File_j->words, File_j->normalizeWords, similarity_matrix);
+
+            File_j->clear();
+            delete File_j;
+            count++;
+         }
+
+         else
+         {
+            // Step 4 :
+            similarity_matrix.push_back({i, j, 0.0});
+         }
+      }
+
+      File_i->clear();
+      delete File_i;
+      count++;
+   }
+
+   cout << endl;
+
+   // Step 4 : Similarity Matrix : sorting.
+   sort(begin(similarity_matrix), end(similarity_matrix), compareMatrix);
+
+   // Step 5 : Reporting top 10 similar pairs of text books.
+   report(similarity_matrix, file_names);
+
+   return 0;
+}
+
+// Main - Function - End
+
+//-------------------------------------------------------------------//
+
+//-------------------------------------------------------------------//
+
+// Class - Function - Definitions - Start
+
+// Reciving and Assigning
+void ParentFile::fileDetails_input(int i, vector<string> Names, vector<string> Paths)
+{
+   fileNumber = i;
+   fileName = Names[fileNumber];
+   filePath = Paths[fileNumber];
+}
+
+// Step 1 : Reading files : filtering required information (alphabetic and numeric characters) , storing capital letter, words in unsorted map.
+void ParentFile::requireWords()
+{
+   ifstream in_file(filePath);
+   if (in_file)
+   {
+      char c;
+      string word;
+      c = in_file.get();
+      while (in_file.good())
+      {
+         // Matching with ASCII chart if character is alphebet or nuemeric , then storing in Vector (string)
+         if ((int(c) >= 65 && int(c) <= 90) || (int(c) >= 97 && int(c) <= 122) || (int(c) >= 48 && int(c) <= 57))
+         {
+            char ch;
+
+            if (islower(c))
+            {
+               ch = toupper(c);
+               word += ch;
+            }
+            else
+            {
+               word += c;
+            }
+         }
+         else
+         {
+            word_i.push_back(word);
+            word.erase();
+         }
+         c = in_file.get();
+      }
+      in_file.close();
+   }
+   // Scaning : word_i vector for word and its reoccurance  and Storing : in Unsorted map
+   for (auto iterator = word_i.begin(); iterator != word_i.end(); iterator++)
+   {
+      unsorted_map[*iterator]++;
+   }
+   totalWords = word_i.size();
+   word_i.clear();
+}
+// Step 1 : Done.
+
+//-------------------------------------------------------------------//
+
+// Step 2 : Removing : commonly occuring words while storing in returnable variables of class ParentFile.
+void ParentFile::RemovingCommon()
+{
+
+   vector<string> common = {"", " ", "A", "AFTER", "ALL", "AN", "AND", "ANY", "ARE", "AS", "AT", "BE", "BEEN", "BEING", "BUT", "BY", "COULD", "DID", "DO", "FOR", "FROM",
+                            "GOOD", "GREAT", "HAD", "HAVE", "HE", "HER", "HIM", "HIS", "I", "IF", "IN", "INTO", "IS", "IT", "MADE", "MAN", "MAY", "ME", "MORE", "MOST", "MY", "NO", "NOT", "NOW",
+                            "OF", "ON", "ONE", "OR", "OUR", "OUT", "OTHER", "S", "SAID", "SHE", "SHOULD", "SO", "SOME", "SUCH", "THAN", "THAT", "THE", "THEIR", "THEM", "THEN", "THERE",
+                            "THESE", "THEY", "THIS", "TIME", "TO", "UP", "UPON", "VERY"
+                                                                                 "WAS",
+                            "WERE", "WE", "WHAT", "WHEN", "WHICH", "WHO", "WILL", "WITH", "WOULD", "YOU"};
+
+   for (auto it : common)
+   {
+      unsorted_map.erase(it);
+   }
+}
+// Step 2 : Done.
+
+//-------------------------------------------------------------------//
+
+// Class - Function - Definitions - End
+// Function - Definitions - Start
+
+// Step 3 : SimilarityIndex
+void similarityIndex(int i, vector<string> &Words_i, vector<float> &normalizeFrequency_i, int j, vector<string> &Words_j, vector<float> &normalizeFrequency_j, vector<similarityMatrix> &matrix)
+{
+   float frequency = 0.00;
+   for (int i = 0; i < 100; i++)
+   {
+      for (int j = 0; j < 100; j++)
+      {
+         if (Words_i[i] == Words_j[j])
+         {
+            frequency += normalizeFrequency_i[i] + normalizeFrequency_i[j];
+         }
+      }
+   }
+   matrix.push_back({i, j, frequency});
+   frequency = 0.00;
+   Words_j.clear();
+   normalizeFrequency_j.clear();
+}
+// Step 3 : Done.
+
+//-------------------------------------------------------------------//
+
+// Step 4 : Similarity Matrix is in main Function.
+// Step 4 : Done.
+
+//-------------------------------------------------------------------//
+
+// Step 5 : Reporting
+void report(vector<similarityMatrix> matrix, vector<string> Book)
+{
+   const char separator = ' ';
+   const int file_nameWidth = 40;
+   const int stringWidth = 22;
+   const int numberWidth = 3;
+   const int file_numberWidth = 4;
+   const int frequencyWidth = 15;
+   const int BookWidth = 9;
+
+   cout << "Top Ten Similar Pair Of Text Books Are : " << endl
+        << endl;
+   for (int i = 0; i < 10; i++)
+   {
+      cout << left << setw(numberWidth) << setfill(separator) << i + 1;
+      cout << left << setw(BookWidth) << setfill(separator) << "Book no.";
+      cout << left << setw(file_numberWidth) << setfill(separator) << matrix[i].i;
+      cout << left << setw(file_nameWidth) << setfill(separator) << Book[matrix[i].i];
+      cout << left << setw(4) << setfill(separator) << "and";
+      cout << left << setw(BookWidth) << setfill(separator) << "Book no.";
+      cout << left << setw(file_numberWidth) << setfill(separator) << matrix[i].j;
+      cout << left << setw(file_nameWidth) << setfill(separator) << Book[matrix[i].j];
+      cout << left << setw(stringWidth) << setfill(separator) << "Similarity value is :";
+      cout << left << setw(frequencyWidth) << setfill(separator) << matrix[i].frequency;
+
+      cout << endl;
+   }
+}
+// Step 5 : Done.
+
+//-------------------------------------------------------------------//
+
+bool comapareSecond(pair<string, int> &a, pair<string, int> &b)
+{
+   return a.second > b.second;
+}
+void ParentFile::storingInClass()
+{
+   // Sorting : storing Unsorted map to another vector of pair for sorting with compareSecond function.
+   for (auto iterator = unsorted_map.begin(); iterator != unsorted_map.end(); iterator++)
+   {
+      sorted_word_and_count.push_back(*iterator);
+   }
+   sort(sorted_word_and_count.begin(), sorted_word_and_count.end(), comapareSecond);
+   for (auto it : sorted_word_and_count)
+   {
+      float normalizeWord = 1.00;
+      normalizeWord = float(it.second / totalWords);
+      words.push_back(it.first);
+      counts.push_back(it.second);
+      normalizeWords.push_back(normalizeWord);
+   }
+   // Clearing : for Reusablity
+   unsorted_map.clear();
+   sorted_word_and_count.clear();
+}
+
+//Data-set entries Storing
+
+void bookDetails(vector<string> &file_names, vector<string> &file_access_paths, int &filesCount)
+{
+
+   if (file_system::exists("..\\similar_books\\Book-Txt\\Book-Txt"))
+   {
+      for (auto &entry : file_system::directory_iterator("..\\similar_books\\Book-Txt\\Book-Txt"))
+      {
+         file_access_paths.push_back(entry.path().string());
+         file_names.push_back(entry.path().filename().string());
+         filesCount++;
+      }
+   }
+   else if (file_system::exists("..\\similar_books\\Book-Txt"))
+   {
+      for (auto &entry : file_system::directory_iterator("..\\similar_books\\Book-Txt"))
+      {
+         file_access_paths.push_back(entry.path().string());
+         file_names.push_back(entry.path().filename().string());
+         filesCount++;
+      }
+   }
+
+   else
+   {
+      string inputlocation;
+      inputlocation = file_system::current_path().string();
+      for (auto &entry : file_system::directory_iterator(inputlocation))
+      {
+         file_access_paths.push_back(entry.path().string());
+         file_names.push_back(entry.path().filename().string());
+         filesCount++;
+      }
+
+      if (file_access_paths.size() > 5)
+      {
+      }
+      else
+      {
+         inputlocation.clear();
+
+         cerr << "Data Set not Found !!" << endl;
+         cout << "Please Enter Data set location to continiue Or Enter 0 (zero) For Exit" << endl;
+
+         cin >> inputlocation;
+
+         if (inputlocation == "0")
+         {
+            exit(1);
+         }
+         else
+         {
+            for (auto &entry : file_system::directory_iterator(inputlocation))
+            {
+               file_access_paths.push_back(entry.path().string());
+               file_names.push_back(entry.path().filename().string());
+               filesCount++;
+            }
+         }
+      }
+   }
+}
+
+//-------------------------------------------------------------------//
+
+// Functions - Definitions - End
---
+++ readme.txt
@@ -0,0 +1,134 @@
+Project No. 1 : Similarity checking from Books.
+
+Object        : To write a C++ program to identify 10 similar pairs of text books int txt format.
+
+References    : Class notes , Professor lectures.
+              : https://cplusplus.com/ , https://stackoverflow.com/ , www.geeksforgeeks.org/
+
+Requirement   : Libraries ==
+
+              : Standard C++ libraries      = <cstring>, <string>, <iostream>, <fstream> <iomanip>
+              : Standard template libraries = <vector>, <map>, <algorithm>
+              : boost                       = <filesystem>
+
+              : Data-Set                    = Book-Txt
+
+Theory        :  A simple (but crude) way to measures similarity is given as following:
+                
+                Step 1 : Identify top 100 frequent (normalize) occurring words in a text file. Consider only
+                 numeric character and alphabet character (by converting both small and capital
+                 alphabets to capital form) of the text file. Normalize the word count of a file by
+                 dividing total number of words in the file.
+
+                Step 2 : "remove commonly occurring six specific words" “a”, “and”, “an” , “of” “in” and
+                   “the” from the file at the time of counting frequent word, total number of word
+                   and the normalize frequency.
+
+                Step 3 : "Similarity index" of two files can be calculated as sum of normalize value of all the
+                  frequent words occurred in both the files.
+
+                Step 4 : "Create a 64x64 size similarity matrix", exclude the self similarity.
+
+               
+                "Report top ten similar pairs of text books using the similarity matrix". 
+
+               
+Procedure    :  
+
+          (i): writing a class ParentFile with filenumber, fileName, filePath, words, counts, totalWords and normalizeWords
+                    variables , their data types are int, string, vector<string>, vector<int>, float, vector<float> respectively.
+             
+         (ii): writing three temporary variables word_i, unsorted_map, sorted_word_and_count.
+             
+        (iii): writing class constructor and destructor.
+             
+         (iv): writing 5 common functions for all text Book Files
+             
+           a): For Receiving : Required information(File : number , name and path) and Assigning.
+               void fileDetails_input(int i, vector<string> Names, vector<string> Paths);
+             :
+
+           b): For step 1 : Reading File : Alphabet or Numeric characters and storing as capital letters Word in vector word_i.
+             : void requireWords();
+
+           c): For Step 2 : Removing Commonly occurring words.
+             : void RemovingCommon();
+
+           d): For Storing , words ,counts and calculated normal frequency and Top occurring words wise sorting.
+             :void storingInClass();
+             
+           e): For Manually clearing data calling when class object is not in use.
+             :void clear()
+
+           v): Step 4 : in main function writing two loops outer loop initialize as i, and inner loop initialize as j
+             : this creating Similarity Matrix(square matrix 64x64) with a condition (i<j) this excludes 
+             : self similarity.
+
+          vi): Step 3 : in this loop creating two files one in outer loop(i) file_i and one inside loop(j) file_j
+             : and in condition calling function
+             : SimilarityIndex()
+             : this function compares Top 100 frequent word of both passed files and calculating sum of 
+             : their normalize frequency and stores in similarity_matrix variable.
+
+         vii): Deleting created class ParentFile Objects(File_i and File_j) accordingly.
+
+             
+Advantages   :
+           i): class    :  having class for same objects reduces code writing.
+          ii): comments :  Comments make code understandable
+         iii): pointer  :  SimilatityIndex function will run for 2080 time in 64*64 matrix(exluding self similarity)
+                           this function is having pointer of  3 vectors (words , count and normal frequency)
+                           insted of real copy vectors. 
+             
+Disadvantages:
+           i): time complixicity : O(n)
+             : in this code map is used to track words reoccurence(counting)
+             : every file is having minimum 40,000 words , and similarityMarix
+             : calling files_i and file_j 2080 times 
+             : This map has to process 40000 * 2000 = 8,00,00,000 words
+             : this makes execution time around 5 minutes to 8 minutes.
+
+Uses         :
+           i): This file is used to identyfy similar par of books
+          ii): This code can be use by any individual to find similar pair of books
+              
+Result       : Top 10 similar pair of books , from given set of 64 text books in txt format
+             : has been identified and reported.
+
+
+
+Top Ten Similar Pair Of Text Books Are :
+
+1  Book no. 7   Gerard's Herbal Vol. 3.txt              and Book no. 10  Gerards Herbal Vol.4.txt                Similarity value is : 0.295295
+2  Book no. 9   Gerards Herbal Vol. 2.txt               and Book no. 10  Gerards Herbal Vol.4.txt                Similarity value is : 0.295048
+3  Book no. 7   Gerard's Herbal Vol. 3.txt              and Book no. 9   Gerards Herbal Vol. 2.txt               Similarity value is : 0.284192
+4  Book no. 7   Gerard's Herbal Vol. 3.txt              and Book no. 8   Gerards Herbal Vol. 1.txt               Similarity value is : 0.25933
+5  Book no. 8   Gerards Herbal Vol. 1.txt               and Book no. 9   Gerards Herbal Vol. 2.txt               Similarity value is : 0.249405
+6  Book no. 8   Gerards Herbal Vol. 1.txt               and Book no. 10  Gerards Herbal Vol.4.txt                Similarity value is : 0.244104
+7  Book no. 5   Foxes Book of Martyrs Part 5.txt        and Book no. 6   Foxes Book of Martyrs Part 6.txt        Similarity value is : 0.219208
+8  Book no. 4   Foxes Book of Martyrs Part 4.txt        and Book no. 6   Foxes Book of Martyrs Part 6.txt        Similarity value is : 0.190478
+9  Book no. 2   Foxes Book of Martyrs Part 2.txt        and Book no. 3   Foxes Book of Martyrs Part 3.txt        Similarity value is : 0.188408
+10 Book no. 4   Foxes Book of Martyrs Part 4.txt        and Book no. 5   Foxes Book of Martyrs Part 5.txt        Similarity value is : 0.187483    
+
+
+
+
+
+
+References : Links
+
+vector erase() and clear() in C++
+https://www.geeksforgeeks.org/vector-erase-and-clear-in-cpp/
+
+map sort by value
+https://www.geeksforgeeks.org/sorting-a-map-by-value-in-c-stl/
+
+How to delete an instance of a class
+https://cplusplus.com/forum/beginner/107822/
+
+How to Sort Data Triplet in a Vector in C++(similarity matrix)
+https://stackoverflow.com/questions/65717961/how-to-sort-a-vector-with-given-column-only-on-a-triplet-value
+
+How to Store Data Triplet in a Vector in C++(similarity matrix)
+https://www.geeksforgeeks.org/store-data-triplet-vector-c
+          
